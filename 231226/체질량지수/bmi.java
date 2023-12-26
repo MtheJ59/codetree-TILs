3145d9c479bd382806b28192013078dfea95bf66
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2023-12-26 19:27:52 +0900

    Title: 체질량지수, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/체질량지수/bmi.java
+++ 231226/체질량지수/bmi.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
 
-        double a = sc.nextInt();
-        double b = sc.nextInt();
-        double c = b / ((a*0.01) * (a*0.01));
-        int d = (int)c;
+        int h, w;
         
+        h = sc.nextInt();
+        w = sc.nextInt();
 
-        if (d >= 25) {
-            System.out.println(d);
-            System.out.println("Obesity");
-        }
+	    int bmi = w * 100 * 100 / (h * h);
+
+        System.out.println(bmi);
+        if(bmi >= 25)
+		    System.out.println("Obesity");
     }
 }
