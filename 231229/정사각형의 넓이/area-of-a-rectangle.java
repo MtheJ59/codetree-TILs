b625fec7f00e05e0ae343c8bf06314ce64d76bb3
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2023-12-29 09:53:00 +0900

    Title: 정사각형의 넓이, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231229/정사각형의 넓이/area-of-a-rectangle.java
+++ 231229/정사각형의 넓이/area-of-a-rectangle.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int t;
+    int a = sc.nextInt();
+    int t;
 
-        if (a <= 5) {
-             t = a * a;
-            System.out.println(t);
-            
-        } else {
-           System.out.println("tiny");
-        }
+    if (a < 5) {
+      t = a * a;
+      System.out.println(t);
+      System.out.println("tiny");
+
+    } else {
+      t = a * a;
+      System.out.println(t);
+
     }
+  }
 }
