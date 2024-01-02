fc8c3cad73dfb888184c18c13aaa9ab2302c25ab
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-02 18:49:51 +0900

    Title: 특정 조건 세 정수 비교, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240102/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -10,23 +10,23 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a >= b && a > c && b > c) {
-            System.out.print("1 ");
-        } else if (a > b && a >= c && b < c) {
-            System.out.print("1 ");
-        } else if (a <= b && a < c && b < c) {
-            System.out.print("1 ");
-        } else if (a < b && a <= c && b > c) {
-            System.out.print("1 ");
-        } else if (a == b && a == c && b == c) {
-            System.out.print("1 ");
-        } else {
-            System.out.print("0 ");
-        }
-        if (a == b && a == c && b == c) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
+        int min = a;
+    
+    if (b < min) {
+      min = b;
     }
+    if (c < min) {
+      min = c;
+    }
+    if (a == min) {
+      System.out.print("1 ");
+    } else {
+      System.out.print("0 ");
+    }
+    if (a == b && a == c && b == c) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+  }
 }
