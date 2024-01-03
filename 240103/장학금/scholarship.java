7b9618db928627bc441566d53d9dcaf9b24416c5
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-03 17:47:05 +0900

    Title: 장학금, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/장학금/scholarship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int scoreA = sc.nextInt();
+        int scoreB = sc.nextInt();
+
+        if (scoreA >= 90) {
+            if (scoreB >= 95) {
+                System.out.println("100000");
+            } else if (scoreB >= 90) {
+                System.out.println("50000");
+            }
+            } else {
+                System.out.println("0");
+        }
+    }
+}
