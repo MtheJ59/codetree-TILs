eb7e2040c711748dd2ed0992a118e2a11bd4d948
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-03 19:01:45 +0900

    Title: 장학금, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240103/장학금/scholarship.java
+++ 240103/장학금/scholarship.java
@@ -8,16 +8,16 @@ public class Main {
 
         int scoreA = sc.nextInt();
         int scoreB = sc.nextInt();
-        int pass;
+        int pass = 0;
 
         if (scoreA >= 90) {
          pass = 1;
         } else {
             pass = 0;
         }
-        if (pass == 1 && pass >= 95) {
+        if (pass == 1 && scoreB >= 95) {
             System.out.println("100000");
-        } else if (pass == 1 && pass >= 90) {
+        } else if (pass == 1 && scoreB >= 90) {
             System.out.println("50000");
         } else {
             System.out.println("0");
