a157dd1b5fa225ec27d3c61dc6ff211668f91cf0
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-08 11:09:30 +0900

    Title: 두 사람, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240108/두 사람/two-person.java
+++ 240108/두 사람/two-person.java
@@ -16,15 +16,15 @@ public class Main {
             System.out.println("1");
         } else if (ageA < 19 && genA != "M" || ageB >= 19 && genB == "M") {
             System.out.println("1");
-        } else if (ageB <19 && genB != "M" || ageA >= 19 && genA == "M") {
+        } else if (ageA >= 19 && genA == "M" || ageB <19 && genB != "M") {
             System.out.println("1");
         } else if (ageA >= 19 && genA != "M" || ageB >= 19 && genB == "M") {
             System.out.println("1");
-        } else if (ageB >= 19 && genB != "M" || ageA >= 19 && genA == "M") {
+        } else if ( ageA >= 19 && genA == "M" ||ageB >= 19 && genB != "M") {
             System.out.println("1");
         } else if (ageA < 19 && genA == "M" || ageB >= 19 && genB == "M") {
             System.out.println("1");
-        } else if (ageB <19 && genB == "M" || ageA >= 19 && genA == "M") {
+        } else if (ageA >= 19 && genA == "M" || ageB <19 && genB == "M") {
             System.out.println("1");
         }
         else {
