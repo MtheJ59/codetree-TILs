5c937a255f6528cf796df03a17ff141df8865291
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-08 18:35:07 +0900

    Title: 윤년인가, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240108/윤년인가/is-leap-year.java
+++ 240108/윤년인가/is-leap-year.java
@@ -8,8 +8,13 @@ public class Main {
 
         int y = sc.nextInt();
 
-        if ((y % 4 == 0) || (y % 4 == 0 && y % 400 == 0 && y % 100 == 0)) {
+        if (y % 4 == 0) {
             System.out.println("true");
+            if (y % 100 == 0) {
+                System.out.println("false");
+            } else if (y % 400 == 0) {
+                System.out.println("true");
+            }
         } else {
             System.out.println("false");
         }
