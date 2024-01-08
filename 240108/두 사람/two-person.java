1d1f11ea7f9885394439e96d2552baa203221393
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-08 11:06:03 +0900

    Title: 두 사람, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240108/두 사람/two-person.java
+++ 240108/두 사람/two-person.java
@@ -22,8 +22,6 @@ public class Main {
             System.out.println("1");
         } else if (ageB >= 19 && genB != "M" || ageA >= 19 && genA == "M") {
             System.out.println("1");
-        } else if (ageA >= 19 && genA != "M" || ageB >= 19 && genB != "M") {
-            System.out.println("1");
         }
 
         else {
