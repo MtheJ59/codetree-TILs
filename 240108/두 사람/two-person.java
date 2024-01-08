30466258dcfb6cc46a6b8160da88b3a3a6c4d0c5
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-08 10:53:55 +0900

    Title: 두 사람, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240108/두 사람/two-person.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int ageA = sc.nextInt();
+        String genA = sc.nextLine();
+        int ageB = sc.nextInt();
+        String genB = sc.nextLine();
+
+    
+
+       if (ageA >= 19 && genA == "M" || ageB >= 19 && genB == "M") {
+            System.out.println("1");
+        } else if (ageA < 19 && genA != "M" || ageB >= 19 && genB == "M") {
+            System.out.println("1");
+        } else if (ageB <19 && genB != "M" || ageA >= 19 && genA == "M") {
+            System.out.println("1");
+        } else if (ageA <= 19 && genA != "M" || ageB >= 19 && genB == "M") {
+            System.out.println("1");
+        } else if (ageB <= 19 && genB != "M" || ageA >= 19 && genA == "M") {
+            System.out.println("1");
+        } else if (ageA >= 19 && genA != "M" || ageB >= 19 && genB != "M") {
+            System.out.println("1");
+        }
+
+        else {
+            System.out.println("0");
+        }
+
+}
+}
