e14feb94574de72b72d90e6a83ad32aa5ba54042
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-09 11:25:33 +0900

    Title: 최댓값 구하기, Time: 205ms, Memory: 10MB, Status: Passed - CodeTree

--- 240109/최댓값 구하기/maximum-value.java
+++ 240109/최댓값 구하기/maximum-value.java
@@ -10,14 +10,14 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a > b) {
-            if (a > c) {
+        if (a >= b) {
+            if (a >= c) {
                 System.out.println(a);
             } else {
                 System.out.println(c);
             }
-        } else if (b > a) {
-            if (b > c) {
+        } else if (b >= a) {
+            if (b >= c) {
                 System.out.println(b);
             } else {
                 System.out.println(c);
