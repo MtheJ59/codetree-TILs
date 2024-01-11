fd5aa0d5d107fb79a2935b05287f97c98e43c2e7
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-11 17:49:20 +0900

    Title: 숫자의 증감, Time: 129ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240111/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240111/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -14,7 +14,7 @@ public class Main {
                 System.out.print(i + " ");
             }
         } else  {
-            for (int i = 1; n <= i; i--) {
+            for (int i = n; i <= 1; i--) {
                 System.out.print(i + " ");
             }
         }
