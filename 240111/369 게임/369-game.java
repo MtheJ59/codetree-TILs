503e07c9ea86ae58353ba126cd2e172484edd362
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-11 18:52:22 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/369 게임/369-game.java
+++ 240111/369 게임/369-game.java
@@ -9,13 +9,9 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) {
+            if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
                 System.out.print(0 + " ");
-            } else if (i % 10 == 3) {
-                System.out.print(0 + " ");
-            } else if (i % 10 == 6) {
-                System.out.print(0 + " ");
-            }  else if (i % 10 == 9) {
+            } else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
                 System.out.print(0 + " ");
             } else {
                 System.out.print(i + " ");
