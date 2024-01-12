0e2758fcafd9fdcf70a870a86ef414d9b0ab9057
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-12 17:58:16 +0900

    Title: 배수의 개수 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240112/배수의 개수 2/number-of-multipliers-2.java
+++ 240112/배수의 개수 2/number-of-multipliers-2.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (int i = 1; i <= 10; i++) {
             a = sc.nextInt();
-            if (i % 2 != 0) {
+            if (a % 2 != 0) {
                  cnt++;
             }
         }
