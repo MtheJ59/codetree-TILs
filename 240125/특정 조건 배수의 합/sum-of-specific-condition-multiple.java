6fcc395914e587db33ecb3a622b0600d21f70aa5
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-01-25 18:04:03 +0900

    Title: 특정 조건 배수의 합, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240125/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240125/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -11,18 +11,23 @@ public class Main {
         int count1 = 0;
         int count2 = 0;
         int count3 = 0;
-       
-       for (int i = a; i <= b; i++) {
-        if (i % 5 == 0) {
-            count1 += i;
-        }
-        for (int j = a; j >= b; j--) {
-            if (i % 5 == 0) {
-                count2 += j;
+
+        if (a > b) {
+            for (int i = a; i >= b; i--) {
+                if (i % 5 == 0) {
+                    count1 += i;
+                }
+            }
+        } else if (b > a) {
+            for (int j = a; j <= b; j++) {
+                if (j % 5 == 0) {
+                    count2 += j;
+                }
             }
         }
-       count3 = count1 + count2;
-       }
-       System.out.print(count3);
-    }
+
+        count3 = count1 + count2;
+         System.out.println(count3);
+        }
+       
 }
