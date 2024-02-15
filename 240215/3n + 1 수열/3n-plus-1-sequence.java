aba92b515398c0a08ec2087b35369261edb12205
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-02-15 17:50:22 +0900

    Title: 3n + 1 수열, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        while (true) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else if (n == 1) {
+                break;
+            } else {
+                n = (n * 3) + 1;
+            }
+            count++;
+        }
+         System.out.print(count);
+    }
+}
