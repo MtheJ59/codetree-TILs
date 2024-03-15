180524678b201f252a5cf95f9d99d90d0728d1d2
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-03-15 10:57:31 +0900

    Title: 합성수 판별기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240315/합성수 판별기/factorization-discriminator.java
+++ 240315/합성수 판별기/factorization-discriminator.java
@@ -9,7 +9,10 @@ public class Main {
         int n = sc.nextInt();
         boolean b = false;
 
-        for (int i = 2; i <= (n + 1); i++) {
+        if (n <= 0) {
+            
+        }
+        for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 b = true;
             }
