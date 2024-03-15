311030dc87777ed771398c916bfadfea44de141a
HyeonJoon Kim <148945586+MtheJ59@users.noreply.github.com>
2024-03-15 10:19:18 +0900

    Title: 합성수 판별기, Time: 126ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240315/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 2) {
+            System.out.print("N");
+        } else if (n % 2 == 0) {
+            System.out.print("C");
+        } else {
+            System.out.print("N");
+        }
+    }
+}
